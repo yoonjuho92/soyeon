1275d723b9490db4e714140248a010e70a8f5afe
책책 <jeong@jeongui-MacBookPro.local>
2024-07-08 16:26:03 +0900

    first commit

---
+++ src/page/Main.jsx
@@ -0,0 +1,145 @@
+import React from 'react';
+
+export default function Main() {
+  return (
+    <div className="main-container w-[1920px] h-[2183px] bg-[#fff] relative overflow-hidden mx-auto my-0">
+      <div className='w-[561px] h-[685px] bg-[url("/src/assets/images/a7993d52b39e1fe89cd6cdd05565dd4b8b6c82fe.png")] bg-cover bg-no-repeat relative z-[3] mt-[-727px] mr-0 mb-0 ml-[-823px]' />
+      <div className="w-[1920px] h-[1084px] relative z-[15] mt-[42px] mr-0 mb-0 ml-0">
+        <div className="w-[1920px] h-[100px] absolute top-0 left-0 z-[15]">
+          <div className='w-[315.061px] h-[50px] bg-[url("/src/assets/images/cf1f4321-c17e-4dfd-ae95-d8a6095ebe09.png")] bg-cover bg-no-repeat absolute top-[26.872px] left-[75px] overflow-hidden z-[22]' />
+          <span className="flex h-[27px] justify-start items-start font-['Pretendard'] text-[24px] font-normal leading-[27px] text-[#eae4ce] absolute top-[36px] left-[1666px] text-left whitespace-nowrap z-[16]">
+            CONTACT
+          </span>
+          <div className="w-[30px] h-[17px] absolute top-[42px] left-[1815px] z-20">
+            <div className='w-[30px] h-[3px] bg-[url("/src/assets/images/eff525b6-b6fd-48f8-a8ea-86e2793f3610.png")] bg-cover bg-no-repeat relative z-[18] mt-[-3px] mr-0 mb-0 ml-0' />
+            <div className='w-[30px] h-[3px] bg-[url("/src/assets/images/099b93ab-3ecf-4447-9b17-5c3946c7e5d6.png")] bg-cover bg-no-repeat relative z-[19] mt-[5px] mr-0 mb-0 ml-0' />
+            <div className='w-[30px] h-[3px] bg-[url("/src/assets/images/2a7af3bc-af6a-43c7-9220-522ff2b0f5f2.png")] bg-cover bg-no-repeat relative z-20 mt-[6px] mr-0 mb-0 ml-0' />
+          </div>
+        </div>
+        <div className="w-[1920px] h-[1080px] bg-[#111111] border-solid border border-[#000] absolute top-[4px] left-0">
+          <div className="w-[928.51px] h-[764.77px] absolute top-[159px] left-[674px] z-[66]">
+            <div className="w-[720px] h-[500px] relative overflow-hidden z-[47] mt-0 mr-0 mb-0 ml-0">
+              <div className="w-[720px] h-[493px] text-[0px] relative overflow-hidden z-[44] mt-[7px] mr-0 mb-0 ml-0">
+                <span className="flex w-[343px] h-[216px] justify-start items-start font-['Pretendard'] text-[60px] font-bold leading-[71.602px] text-[#349b60] relative text-left overflow-hidden z-[46] mt-[45px] mr-0 mb-0 ml-[52px]">
+                  EDUCATION
+                  <br />
+                  AWARDS
+                  <br />
+                  CAREER
+                </span>
+                <div className='w-full h-full bg-[url("/src/assets/images/59a912e9-5eb7-42ee-8abc-b49483227463.png")] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-0 z-[45]' />
+              </div>
+            </div>
+            <div className='w-[478.51px] h-[258.91px] bg-[url("/src/assets/images/7bc91b4c-e9a3-4df1-b0e1-5abb2b5c2871.png")] bg-cover bg-no-repeat absolute top-[588px] left-[450px] overflow-hidden z-[65]' />
+            <div className="w-[373px] h-[173.77px] absolute top-[591px] left-0 overflow-hidden z-[66]">
+              <div className='w-[337.02px] h-[3px] bg-[url("/src/assets/images/a1f728d1-92b7-414b-948f-dd90b5c5fe1b.png")] bg-[length:100%_100%] bg-no-repeat relative z-[68] mt-[10.48px] mr-0 mb-0 ml-[17.99px]' />
+              <span className="block h-[60px] font-['Pretendard'] text-[50px] font-bold leading-[59.668px] text-[#349b60] relative text-left whitespace-nowrap z-[70] mt-[43.52px] mr-0 mb-0 ml-[67px]">
+                CONTACT
+              </span>
+              <div className='w-[337.02px] h-[3px] bg-[url("/src/assets/images/a669786d-7ef5-488b-9cf0-d6af3dc6b1d2.png")] bg-[length:100%_100%] bg-no-repeat relative z-[69] mt-[41.79px] mr-0 mb-0 ml-[17.99px]' />
+              <div className='w-full h-full bg-[url("/src/assets/images/dd7fd4b8-13ca-4690-aae4-7902b43eda84.png")] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-0 z-[67]' />
+            </div>
+          </div>
+
+          <div className="perspective-1000 cursor-pointer">
+            <div className="w-[570px] h-[650px] top-[227px] left-[74px] relative transform-style-3d transition-transform duration-700 transform text-white hover:rotate-y-180">
+              <div className="w-full h-full backface-hidden absolute inset-0 rotate-y-0 text-[0px] absolute overflow-hidden z-[5]">
+                <span className="block h-[72px] font-['Pretendard'] text-[60px] font-bold leading-[71.602px] text-[#eae4ce] relative text-left whitespace-nowrap z-[13] mt-[54px] mr-0 mb-0 ml-[54px]">
+                  PROFILE
+                </span>
+                <div className="w-full h-full absolute top-0 left-0 z-[6]">
+                  <div className="w-full h-full absolute top-0 left-0 z-[7]">
+                    <div className="w-[568.956px] h-[650px] relative z-[9] mt-0 mr-0 mb-0 ml-[0.52px]">
+                      <div className="w-full h-full absolute top-0 left-0 z-10"></div>
+                    </div>
+                    <div className='w-full h-full bg-[url("/src/assets/images/c7714382-56b6-4c6a-9af6-a540f549e3f8.png")] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-0 z-[8]' />
+                  </div>
+                </div>
+              </div>
+              <div className='backface-hidden w-full h-full absolute inset-0 rotate-y-180 bg-[url("/src/assets/images/5a33f2d03c5de361a08cca849152b22f8bf2d5f8.png")] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-0 z-[8]' />
+            </div>
+          </div>
+          <div className="w-[425px] h-[558px] bg-[#ff8650] absolute top-[249px] left-[1424px] shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] z-[55]">
+            <div className="w-full h-full absolute top-0 left-0 overflow-hidden z-[57]">
+              <span className="flex h-[12.9%] justify-start items-start font-['Pretendard'] text-[60px] font-bold leading-[71.602px] text-[#fffaeb] absolute top-[8.06%] left-[12.47%] text-left whitespace-nowrap z-[56]">
+                SKILLS
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-[2294px] h-[445px] relative z-[25] mt-[-4px] mr-0 mb-0 ml-[-374px]">
+        <div className="w-[960px] h-[445px] text-[0px] bg-[#ff8650] absolute top-0 left-[374px] z-[25]">
+          <span className="font-['Pretendard'] text-[250px] font-semibold leading-[298.34px] text-[#fffaeb] relative text-left">
+            E
+          </span>
+          <span className="opacity-0 hover:opacity-100 transition-opacity duration-300 font-['Pretendard'] text-[180px] font-semibold leading-[298.34px] text-[#fffaeb] relative text-left">
+            ditorial
+          </span>
+          <div className="w-[900px] h-[86px] relative overflow-hidden z-[27] mt-[28px] mr-0 mb-0 ml-[60px]">
+            <div className='w-[98.47%] h-full bg-[url("/src/assets/images/f0208f22-27d5-4256-adfa-ec49bf2a28bc.png")] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-[1.53%] z-[28]' />
+            <div className="w-[98.47%] h-full absolute top-0 left-[1.53%] z-[29]">
+              <div className="w-[900px] h-[55.444px] relative z-30 mt-[16.7px] mr-0 mb-0 ml-[-13.781px]">
+                <div className='w-[886.219px] h-[7px] bg-[url("/src/assets/images/f796cf44-d14a-4591-b55b-45333bebb935.png")] bg-[length:100%_100%] bg-no-repeat relative z-[34] mt-[-3.5px] mr-0 mb-0 ml-[13.781px]' />
+                <div className='w-[886.219px] h-[7px] bg-[url("/src/assets/images/cfeb3a63-5a89-4693-a04d-50097311cf8a.png")] bg-[length:100%_100%] bg-no-repeat relative z-[33] mt-[11.481px] mr-0 mb-0 ml-[13.781px]' />
+                <div className='w-[886.219px] h-[7px] bg-[url("/src/assets/images/89292ffc-d4cc-41b8-8a9c-f84f74cafe83.png")] bg-[length:100%_100%] bg-no-repeat relative z-[32] mt-[11.481px] mr-0 mb-0 ml-[13.781px]' />
+                <div className='w-[886.219px] h-[7px] bg-[url("/src/assets/images/bc4aae9c-caac-4797-ab47-8a4bc0330068.png")] bg-[length:100%_100%] bg-no-repeat relative z-[31] mt-[11.481px] mr-0 mb-0 ml-[13.781px]' />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-[960px] h-[445px] bg-[#fffaeb] absolute top-0 left-[1334px] z-[23]">
+          <span className="flex h-[167px] justify-start items-center font-['Pretendard'] text-[250px] font-semibold leading-[167px] text-[#349b60] absolute top-[139px] left-[59px] text-left whitespace-nowrap z-[41]">
+            B
+          </span>
+        </div>
+        <span className="flex w-[194px] h-[40px] justify-center items-start font-['Pretendard'] text-[36px] font-bold leading-[40px] text-[#797979] absolute top-[74px] left-0 text-center whitespace-nowrap z-[1]">
+          About
+        </span>
+        <span className="flex w-[194px] h-[40px] justify-center items-start font-['Pretendard'] text-[36px] font-bold leading-[40px] text-[#797979] absolute top-[214px] left-0 text-center whitespace-nowrap z-[2]">
+          Skill
+        </span>
+      </div>
+      <div className="w-[1920px] h-[445px] relative z-[26] mt-0 mr-0 mb-0 ml-0">
+        <div className="w-[960px] h-[445px] bg-[#349b60] absolute top-0 left-0 z-[26]">
+          <span className="flex h-[167px] justify-start items-center font-['Pretendard'] text-[250px] font-semibold leading-[167px] text-[#111111] absolute top-[139px] left-[64px] text-left whitespace-nowrap z-40">
+            G
+          </span>
+        </div>
+        <div className="w-[960px] h-[445px] bg-[#111111] absolute top-0 left-[960px] z-[24]">
+          <div className="flex w-[81px] h-[17px] justify-between items-center relative z-[38] mt-[26px] mr-0 mb-0 ml-[27px]">
+            <div className='w-[17px] h-[17px] shrink-0 bg-[url("/src/assets/images/8360102b-7f6a-4ba9-8fe4-013835768a6f.png")] bg-cover bg-no-repeat rounded-[50%] relative z-[36]' />
+            <div className='w-[17px] h-[17px] shrink-0 bg-[url("/src/assets/images/3721749c-27f4-473c-86eb-94a3108d3a02.png")] bg-cover bg-no-repeat rounded-[50%] relative z-[37]' />
+            <div className='w-[17px] h-[17px] shrink-0 bg-[url("/src/assets/images/c55b619f-dba5-449b-b9d7-bdc4cd612503.png")] bg-cover bg-no-repeat rounded-[50%] relative z-[38]' />
+          </div>
+          <div className='w-[960px] h-[4px] bg-[url("/src/assets/images/974444e0-89d4-43c5-b8b5-d12fdcfc0fe8.png")] bg-cover bg-no-repeat relative z-[35] mt-[22px] mr-0 mb-0 ml-0' />
+          <span className="block h-[167px] font-['Pretendard'] text-[250px] font-semibold leading-[167px] text-[#ff8650] relative text-left whitespace-nowrap z-[42] mt-[83px] mr-0 mb-0 ml-[71px]">
+            W
+          </span>
+        </div>
+      </div>
+      <div className="w-[1920px] h-[211px] text-[0px] bg-[#fffaeb] relative z-[77] mt-[2px] mr-0 mb-0 ml-0">
+        <span className="block h-[27px] font-['Pretendard'] text-[24px] font-semibold leading-[27px] text-[#111111] relative text-left whitespace-nowrap z-[75] mt-[62px] mr-0 mb-0 ml-[1756px]">
+          Contact
+        </span>
+        <span className="flex w-[120px] h-[27px] justify-end items-center font-['Pretendard'] text-[18px] font-semibold leading-[21.48px] text-[#111111] relative text-right whitespace-nowrap z-[76] mt-[23px] mr-0 mb-0 ml-[1725px]">
+          IG
+        </span>
+        <span className="flex w-[120px] h-[27px] justify-end items-center font-['Pretendard'] text-[18px] font-semibold leading-[21.48px] text-[#111111] relative text-right whitespace-nowrap z-[77] mt-0 mr-0 mb-0 ml-[1725px]">
+          Mail
+        </span>
+        <div className="w-[431px] h-[27px] font-['Pretendard'] text-[24px] font-semibold leading-[27px] absolute top-[92px] left-[744px] text-left whitespace-nowrap z-[74]">
+          <span className="font-['Pretendard'] text-[24px] font-semibold leading-[28.641px] text-[#111111] relative text-left">
+            2024 Kim Soyeon. All Rights Reserve
+          </span>
+          <span className="font-['Pretendard'] text-[24px] font-semibold leading-[28.641px] text-[#111111] tracking-[0.24px] relative text-left">
+            d.
+          </span>
+        </div>
+        <span className="flex h-[27px] justify-start items-center font-['Pretendard'] text-[24px] font-semibold leading-[27px] text-[#111111] absolute top-[92px] left-[75px] text-left whitespace-nowrap z-[73]">
+          noeyosmig@gmail.com
+        </span>
+      </div>
+    </div>
+  );
+}
